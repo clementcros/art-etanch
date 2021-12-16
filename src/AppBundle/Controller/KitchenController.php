a1049778aed4e0674370d8b185cc85b7defca59b
clcro <clement.cros@smile.fr>
2021-12-16 23:56:09 +0100

    - add kitchen template and relation

---
+++ src/AppBundle/Controller/KitchenController.php
@@ -0,0 +1,69 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Utils\SearchHelper;
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use eZ\Publish\Core\MVC\Symfony\View\ContentView;
+
+class KitchenController extends Controller
+{
+
+
+    /**
+     * @var SearchHelper
+     */
+    private $searchHelper;
+
+    /**
+     * @param SearchHelper $searchHelper
+     */
+    public function __construct(
+        SearchHelper $searchHelper
+    )
+    {
+        $this->searchHelper = $searchHelper;
+    }
+
+    /**
+     * @param ContentView $view
+     * @return ContentView
+     */
+    public function fullAction(ContentView $view): ContentView
+    {
+
+        $imageRelations = $this->searchHelper->locationsList(
+            $view->getLocation()->id,
+            ['image'],
+            []
+        );
+
+        $images = $this->getContentLocationList($imageRelations);
+
+        $kitchensRelations = $this->searchHelper->getTargetOfRelations(
+            $view->getContent(),
+            'kitchen_relation',
+            true,
+            false
+        );
+
+        $view->addParameters([
+            'images' => $images,
+            'kitchensRelations' => $kitchensRelations,
+        ]);
+
+        return $view;
+    }
+
+    /**
+     * @param $list
+     * @return array
+     */
+    private function getContentLocationList($list) {
+        $data = array();
+        foreach ($list as $menuList) {
+            array_push($data,$menuList->getContent());
+        }
+        return $data;
+    }
+
+}
